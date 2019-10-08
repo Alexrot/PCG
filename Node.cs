094fa27faf0260b86f2fc1209d45cebeca1dad42
Alexrot <g.megaro1@studenti.unisa.it>
2019-10-08 16:45:04 +0200

    Add files via upload
    grafi

---
+++ Node.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node 
+{
+    public Vector2f position;
+    public Arc[] edges;
+    public int countArc;
+    public Node(Vector2f position)
+    {
+        this.position = position;
+        edges = new Arc[99];
+        countArc = 0;
+    }
+
+
+    public void AddEdge(Node a, Node b)
+    {
+        edges[countArc] = new Arc(2, a, b);
+
+    }
+
+    public void RemuveEdge()
+    {
+
+    }
+}
